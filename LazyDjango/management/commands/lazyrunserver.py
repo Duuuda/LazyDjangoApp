985cb8ba9f5af7f7d3fd53143e821735d5a90c74
Duuuda <60673731+Duuuda@users.noreply.github.com>
2021-02-13 12:58:32 +0300

    Add args to lazyrunserver.py

--- LazyDjango/management/commands/lazyrunserver.py
+++ LazyDjango/management/commands/lazyrunserver.py
@@ -5,8 +5,29 @@ from os import system
 class Command(BaseCommand):
     help = 'This function prepares and launches Django without user intervention'
 
+    def add_arguments(self, parser):
+        parser.add_argument('-host', '--host',
+                            action='store', default='127.0.0.1',
+                            help='Start your project on this address')
+        parser.add_argument('-p', '--port',
+                            action='store', default='8000',
+                            help='Start your project on this port')
+        parser.add_argument('-ncs', '--no_collect_static',
+                            action='store_true', default=False,
+                            help='Start your project without collecting static')
+        parser.add_argument('-nrm', '--no_re_migrate',
+                            action='store_true', default=False,
+                            help='Start your project without re-migrate')
+        parser.add_argument('-ncdu', '--no_check_default_user',
+                            action='store_true', default=False,
+                            help='Start your project without checking default user in db')
+
     def handle(self, *args, **options):
-        system('python manage.py collectstatic --no-input')
-        system('python manage.py remigrate')
-        system('python manage.py checkdefaultuser')
-        system('python manage.py runserver')
+        if not options['no_collect_static']:
+            system('python manage.py collectstatic --no-input')
+        if not options['no_re_migrate']:
+            system('python manage.py remigrate')
+        if not options['no_check_default_user']:
+            system('python manage.py checkdefaultuser')
+
+        system(f'python manage.py runserver {options["host"]}:{options["port"]}')
